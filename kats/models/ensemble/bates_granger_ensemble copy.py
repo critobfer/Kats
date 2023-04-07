ff44e4d8fc25ccb693c74728b0571b3ea00ac8c1
Cristina <ctff000@hotmail.com>
2023-04-07 23:45:53 +0200

    UPDATE: intento incorporar elección de backtester

---
+++ kats/models/ensemble/bates_granger_ensemble copy.py
@@ -0,0 +1,219 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Ensemble models Bates & Granger individual models
+
+Assume we have k base models, after we make forecasts with each individual
+model, Computes forecast combination weights according to the approach by 
+Bates and Granger (1969) and produces a forecast.
+"""
+import logging
+import sys
+from multiprocessing import cpu_count, Pool
+from typing import Any, cast, Dict, List, Optional, Type, Union
+
+import numpy as np
+import pandas as pd
+from kats.consts import Params, TimeSeriesData
+from kats.models.ensemble import ensemble
+from kats.models.ensemble.ensemble import BASE_MODELS, EnsembleParams
+from kats.models.model import Model
+from kats.utils.backtesters import BackTesterSimple, BackTesterExpandingWindow, BackTesterRollingWindow, BackTesterFixedWindow
+
+class BatesGrangerEnsemble(ensemble.BaseEnsemble):
+    """Bates & Granger ensemble model class
+
+    Attributes:
+        data: the input time series data as in :class:`kats.consts.TimeSeriesData`
+        params: the model parameter class in Kats
+    """
+
+    freq: Optional[str] = None
+    errors: Optional[Dict[str, Any]] = None
+    weights: Optional[Dict[str, Any]] = None
+    fcst_weighted: Optional[Union[pd.DataFrame, pd.Series]] = None
+    fcst_dates: Optional[pd.DatetimeIndex] = None
+    dates: Optional[pd.DatetimeIndex] = None
+    fcst_df: Optional[pd.DataFrame] = None
+
+    def __init__(self, data: TimeSeriesData, params: EnsembleParams, back_method: str) -> None:
+        self.data = data
+        self.params = params
+        self.back_method = back_method
+        if not isinstance(self.data.value, pd.Series):
+            msg = "Only support univariate time series, but get {type}.".format(
+                type=type(self.data.value)
+            )
+            logging.error(msg)
+            raise ValueError(msg)
+
+    def _backtester_single(
+        self,
+        params: Params,
+        # pyre-fixme[24]: Generic type `Model` expects 1 type parameter.
+        model_class: Type[Model],
+        start_train_percentage: int = 80,
+        end_train_percentage: int = 80,
+        expanding_steps: int = 2,
+        sliding_steps: int = 2,
+        train_percentage: int = 80,
+        test_percentage: int = 20,
+        window_percentage: int = 20,
+        err_method: str = "mse"
+        # back_method: str = self.back_method
+    ) -> float:
+        """Private method to run all backtesting process
+
+        Args:
+            params: Kats model parameters
+            model_class: Untyped. Defines type of model
+            train_percentage: float. Percentage of data used for training
+            test_percentage: float. Percentage of data used for testing
+            error_method: list of strings indicating which errors to calculate
+                we currently support "mape", "smape", "mae", "mase", "mse", "rmse"
+
+        Returns:
+            float, the backtesting error
+        """
+        if self.back_method not in ["simple","expanding","rolling","fixed"]:
+            return print("This method are not available, chooose  simple, expanding, rolling or fixed")
+        if self.back_method == "simple":
+            bt = BackTesterSimple(
+                [err_method],
+                self.data,
+                params,
+                train_percentage,
+                test_percentage,
+                model_class,
+            )
+        if self.back_method == "expanding":
+            bt = BackTesterExpandingWindow(
+                [err_method],
+                self.data,
+                params,
+                start_train_percentage,
+                end_train_percentage,
+                train_percentage,
+                expanding_steps,
+                test_percentage,
+                model_class,
+            )
+        if self.back_method == "rolling":
+            bt = BackTesterRollingWindow(
+                [err_method],
+                self.data,
+                params,
+                train_percentage,
+                test_percentage,
+                sliding_steps,
+                model_class,
+            )
+            
+        if self.back_method == "fixed":
+            bt = BackTesterFixedWindow(
+                [err_method],
+                self.data,
+                params,
+                train_percentage,
+                test_percentage,
+                window_percentage,
+                model_class,
+            )
+        bt.run_backtest()
+        return bt.get_error_value(err_method)
+
+    def _backtester_all(self, err_method: str = "mse") -> Dict[str, Any]:
+        """Private method to run all backtesting process
+
+        Args:
+            error_method: list of strings indicating which errors to calculate
+                we currently support "mape", "smape", "mae", "mase", "mse", "rmse"
+
+        Returns:
+            Dict of errors from each model
+        """
+
+        num_process = min(len(BASE_MODELS.keys()), (cpu_count() - 1) // 2)
+        if num_process < 1:
+            num_process = 1
+        pool = Pool(processes=(num_process), maxtasksperchild=1000)
+        backtesters = {}
+        for model in self.params.models:
+            backtesters[model.model_name] = pool.apply_async(
+                self._backtester_single,
+                args=(model.model_params, BASE_MODELS[model.model_name.lower()]),
+                kwds={"err_method": err_method},
+            )
+        pool.close()
+        pool.join()
+        self.errors = {model: res.get() for model, res in backtesters.items()}
+        original_weights = {
+            model: 1/(err**2 + sys.float_info.epsilon) # El mse
+            for model, err in self.errors.items()
+        }
+        self.weights = {
+            model: err / sum(original_weights.values()) 
+            # El peso original es el mse, si lo dividimos por la suma de los pesos tenemos el valor final
+            for model, err in original_weights.items()
+        }
+        return self.weights
+
+    # pyre-fixme[14]: `predict` overrides method defined in `Model` inconsistently.
+    # pyre-fixme[2]: Parameter must be annotated.
+    # pyre-fixme[15]: `predict` overrides method defined in `Model` inconsistently.
+    def predict(self, steps: int, **kwargs) -> pd.DataFrame:
+        """Predict method of weighted average ensemble model
+
+        Args:
+            steps: the length of forecasting horizon
+
+        Returns:
+            forecasting results as in pd.DataFrame
+        """
+        # keep these in kwargs to pass to _predict_all.
+        self.freq = freq = kwargs.get("freq", "D")
+        err_method = kwargs.get("err_method", "mse")
+        # calculate the weights
+        self._backtester_all(err_method=err_method)
+
+        # fit model with all available time series
+        pred_dict = self._predict_all(steps, **kwargs)
+
+        fcst_all = pd.concat(
+            # pyre-fixme[16]: `Model` has no attribute `fcst`.
+            [x.fcst.reset_index(drop=True) for x in pred_dict.values()],
+            axis=1,
+            copy=False,
+        )
+        fcst_all.columns = cast(List[str], pred_dict.keys()) 
+        # Lista de string y los modelos(elemento),lo convierte en este formato
+        weights = self.weights 
+        assert weights is not None #Si es None ==> AssertionError
+        # PREDICCIÓN FINAL:
+        self.fcst_weighted = fcst_all.dot(np.array(list(weights.values()))) # Producto matricial
+
+        # create future dates
+        last_date = self.data.time.max()
+        dates = pd.date_range(start=last_date, periods=steps + 1, freq=freq)
+        dates = dates[dates != last_date]
+        self.fcst_dates = dates.to_pydatetime()
+        self.dates = dates[dates != last_date]
+        self.fcst_df = fcst_df = pd.DataFrame(
+            {"time": self.dates, "fcst": self.fcst_weighted}, copy=False
+        )
+
+        logging.debug("Return forecast data: {fcst_df}")
+        return fcst_df
+
+    def __str__(self) -> str:
+        """Get default parameter search space for the Bates & Gates ensemble model
+
+        Args:
+            None
+
+        Returns:
+            Model name as a string
+        """
+        return "Bates & Granger Ensemble"
